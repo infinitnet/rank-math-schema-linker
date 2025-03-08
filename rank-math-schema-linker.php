92000eeb0ed27f8807bbf0705c5779b4b66a134b
infinitnet <6189915+infinitnet@users.noreply.github.com>
2025-03-08 21:42:25 +0100

    ver

--- rank-math-schema-linker.php
+++ rank-math-schema-linker.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Rank Math Schema Linker
  * Description: Adds significant and related links to Rank Math's WebPage schema
- * Version: 1.0.6
+ * Version: 1.0.7
  * Author: Infinitnet
  * Text Domain: rank-math-schema-linker
  * License: GPL v2 or later
